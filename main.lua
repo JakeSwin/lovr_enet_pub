fc223fdc7335c4cd8dbe2af1cf767da068af1152
Jacob Swindell <jacob.swindell@btinternet.com>
2025-05-13 09:10:31 +0100

    add enet pub

--- main.lua
+++ main.lua
@@ -1,3 +1,45 @@
+local enet = require 'enet'
+local messagepack = require 'MessagePack'
+
+local host
+local server
+local peer
+
+function lovr.load()
+    host = enet.host_create()
+    server = host:connect('192.168.1.165:6789')
+    messagepack.set_number('float')
+    print("attempting to connect to: 192.168.1.165:6789")
+end
+
+function lovr.update(dt)
+    local event = host:service(0)
+    if event then
+        if event.type == 'connect' then
+            print("Successfully connected to: 192.168.1.165:6789")
+            peer = event.peer
+            print("Set peer variable")
+        end
+    end
+
+    if peer then
+        local head_x, head_y, head_z, lhand_x, lhand_y, lhand_z, rhand_x, rhand_y, rhand_z
+
+        head_x, head_y, head_z = lovr.headset.getPosition()
+        lhand_x, lhand_y, lhand_z = lovr.headset.getPosition("hand/left")
+        rhand_x, rhand_y, rhand_z = lovr.headset.getPosition("hand/right")
+
+        local data = messagepack.pack({
+            head_x, head_y, head_z,
+            lhand_x, lhand_y, lhand_z,
+            rhand_x, rhand_y, rhand_z
+        })
+
+        peer:send(data)
+        print("Sending packet")
+    end
+end
+
 function lovr.draw(pass)
     for i, hand in ipairs(lovr.headset.getHands()) do
         local x, y, z = lovr.headset.getPosition(hand)
